fb0a7e5b179afdcc4d70483e76820be2417eea9e
Wei Luo <ryan.luo89@gmail.com>
2016-03-13 13:39:29 -0700

    Skynet: the Virus - Rename Class

--- src/com/self/codingame/SkynetVirusPlayer.java
+++ src/com/self/codingame/SkynetVirusPlayer.java
@@ -9,7 +9,7 @@ import java.math.*;
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
-class Player {
+class SkynetVirusPlayer {
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);

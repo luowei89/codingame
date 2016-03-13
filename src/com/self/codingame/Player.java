7050d0346ed2d6ff616274fd98b5940a1ed3740b
Wei Luo <ryan.luo89@gmail.com>
2016-03-13 13:34:12 -0700

    Skynet: the Virus - Update
    remove link next to start
    remove link to gateways that have more links first

--- src/com/self/codingame/Player.java
+++ src/com/self/codingame/Player.java
@@ -29,6 +29,8 @@ class Player {
             gateways.add(EI);
         }
 
+        arrangeAdjList(adjList);
+
         // game loop
         while (true) {
             int SI = in.nextInt(); // The index of the node on which the Skynet agent is positioned this turn
@@ -56,23 +58,92 @@ class Player {
 
     public static String bfSearch(int start, Map<Integer, List<Integer>> adjList, Set<Integer> gateways) {
         Queue<Integer> queue = new LinkedList<>();
-        Set<Integer> explored = new HashSet<>();
         queue.add(start);
-        explored.add(start);
-        while (!queue.isEmpty()) {
+        queue.add(null);
+        Map<Integer, Integer> paths = new HashMap<>();
+        paths.put(start, null);
+        Map<Integer, String> candidates = new HashMap<>();
+        while (queue.size() > 1) {
             Integer node = queue.poll();
-            List<Integer> list = adjList.get(node);
-            if (list != null) {
-                for (int x : list) {
-                    if (gateways.contains(x)) {
-                        removeFromList(adjList, node, x);
-                        return node + " " + x;
-                    } else if (!explored.contains(x)) {
-                        queue.add(x);
+            if (node == null) {
+                if (!candidates.isEmpty()) {
+                    int maxLinks = -1;
+                    String bestCandidate = "";
+                    for (int key : candidates.keySet()) {
+                        List<Integer> linksList = adjList.get(key);
+                        int links = linksList == null ? 0 : linksList.size();
+                        if (links > maxLinks) {
+                            maxLinks = links;
+                            bestCandidate = candidates.get(key);
+                        }
+                    }
+                    String[] nodes = bestCandidate.split(" ");
+                    removeFromList(adjList, Integer.parseInt(nodes[0]), Integer.parseInt(nodes[1]));
+                    return bestCandidate;
+                }
+                queue.add(null);
+            } else {
+                List<Integer> list = adjList.get(node);
+                Integer path = paths.get(node);
+                if (list != null) {
+                    for (int x : list) {
+                        int current = path == null ? x : path;
+                        if (gateways.contains(x) && !candidates.containsKey(x)) {
+                            candidates.put(x, start + " " + current);
+                        }
+                        if (!paths.containsKey(x)) {
+                            queue.add(x);
+                            paths.put(x, current);
+                        }
                     }
                 }
             }
         }
         return "";
+    }
+
+    public static void arrangeAdjList(Map<Integer, List<Integer>> adjList) {
+        for (int key : adjList.keySet()) {
+            List<Integer> list = adjList.get(key);
+            List<Integer> newList = arrangeList(list, adjList);
+            adjList.put(key, newList);
+        }
+    }
+
+    public static List<Integer> arrangeList(List<Integer> list, Map<Integer, List<Integer>> adjList) {
+        PriorityQueue<Node> pQueue = new PriorityQueue<>();
+        List<Integer> newList = new ArrayList<>();
+        for (int node : list) {
+            List<Integer> linkList = adjList.get(node);
+            int links = linkList== null ? 0 : linkList.size();
+            pQueue.add(new Node(node, links));
+        }
+        while (!pQueue.isEmpty()) {
+            newList.add(pQueue.poll().getId());
+        }
+        return newList;
+    }
+
+    public static class Node implements Comparable {
+        private int id;
+        private int links;
+
+        public Node(int id, int links) {
+            this.id = id;
+            this.links = links;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            return ((Node)o).getLinks() - this.links;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public int getLinks() {
+            return links;
+        }
     }
 }
